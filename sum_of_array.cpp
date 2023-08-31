850ce2ab6cc015417712e6b85d192297647c3ae7
aniket8286 <aniketchauhan8286@gmail.com>
2023-08-31 07:25:35 +0530

    cpp added

---
+++ sum_of_array.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+
+void quickSort(){
+    
+}
+
+void BubbleSort(){
+ 
+
+}
+
+void mergeSort(){
+    
+}
+
+
+int sum (int a[], int n){
+int ans=0;
+for(int i=0; i <n; i++){
+    
+    ans=ans +a[i]; 
+}
+    cout<<"sum of the number is "<<ans<<endl;
+
+    cout<<endl;
+    return ans;
+}
+
+
+
+
+int main(){
+
+int a[5]={1,2,3,4,5};
+
+int b[10]={12,45,12,-34,56,-32,90,-45,80,56};
+
+sum(a,5);
+sum(b,10);
+
+
+
+
+    return 0;
+}
